078a4ef619b2d00c5e055516e34610079a9f00d9
fawziya zeyada <100339806+FawziyaMuhammed25@users.noreply.github.com>
2024-08-24 20:36:02 +0300

    DataSec1.js

---
+++ src/DataSec1.js
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+export const DataSec1  =[
+    {
+        id:1,
+        name :"tech",
+        title: "here's the first artical",
+        icon:  <FontAwesomeIcon icon={faClock} className=' align-self-center' /> ,
+        time : "2 h ago",
+       
+        text :" Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus cupiditate ipsa laborum, quae consequatur sed ab exercitationem quas? Nobis doloribus soluta repellat at repudiandae accusamus deleniti repellendus, facilis blanditiis provident laborum esse enim, in quaerat a?"
+    },
+  
+  
+
+];
