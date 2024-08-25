3a928e86b5ab5116c99852a8bb360e2bf38de28f
fawziya zeyada <100339806+FawziyaMuhammed25@users.noreply.github.com>
2024-08-25 11:13:35 +0300

    Update section1.jsx

--- src/components/section1.jsx
+++ src/components/section1.jsx
@@ -6,106 +6,107 @@ import Section2 from './section2';
 const Section1 = () => {
 
   return (
-        <>
-    <div className='text-white'>
-  
-<div className="container">
- 
-    
-          <div className=''>
-          {
-          DataSec1.map((item) => (
-            <div key={item.id}  className='row my-4 '>
-              
-              <div className=" col-lg-6 col-md-12 div1 d-flex ">
-              <div className="">
+       
+     <div className='mb'>
+        <div className='bg '>
+<div className='text-white overLay '>
+   <div className="container bg-container position-relative ">
+
+   <div className=''>
+      {
+      DataSec1.map((item) => (
+        <div key={item.id}  className='row   '>
+          
+          <div className=" col-lg-7 col-md-12  d-flex  div1 ">
+          <div className="col-lg-2">
+          <div className="position-relative  w ">
           <aside className='vertical-text'>
-             <h1>Featured News</h1>
+            <h1>Featured News</h1>
           </aside>
         </div>
-            <div className=" p-4   div2">
-              
-              <div className="div2">
-              <h4 >{item.name}</h4>
-              <div className="line mb-2 " >
-               </div>
-               <div className="text row  position-relative ">
-                              <div className="  col-8 title">
-                                  <h3 >{item.title} </h3> 
-                              </div>
-                              <div className='d-flex justify-content-end  col-4 clck '>
-                              {item.icon}
-                                <span className='align-self-center ps-2'> {item.time}  </span>
-                              </div>
-                              <p>
-                                  {item.text}
-                              </p>
-                </div>
-              </div>
-           
           </div>
-              </div>
-              <div className="col-lg-6 col-md-12 div1 ">
-              <div className=" p-4  ">
-              <div className=" col-12  div1 ">
-              <h4>{item.name}</h4>
-              <div className="line mb-2 " >
-               </div>
-               <div className="text row position-relative  ">
-                              <div className="  col-lg-9 col-md-12 title ">
-                                  <h3 >{item.title}</h3> 
-                              </div>
-                              <div className='d-flex justify-content-end  col-lg-3 col-md-12 clck '>
-                              {item.icon}
-                                <span className='align-self-center ps-2'> {item.time}  </span>
-                              </div>
-                              <p>
-                            {item.text}
-                          </p>
-                </div>
-              </div>
-              <div className=" col-12 div1 ">
-              <h4>{item.name}</h4>
-              <div className="line mb-2 " >
-               </div>
-               <div className="text row   ">
-                              <div className="  col-lg-9 col-md-12 title">
-                                  <h3 >{item.title} </h3> 
-                              </div>
-                              <div className='d-flex justify-content-end  col-lg-3 col-md-12  '>
-                              {item.icon}
-                                <span className='align-self-center ps-2'> {item.time}  </span>
-                              </div>
-                              <p>
-                             {item.text}
-                          </p>
-                </div>
-              </div>
-           
-          </div>
+        <div className=" p-4   ">
           
-             </div>
-
-                       
+          <div className="">
+          <h4 >{item.name}</h4>
+          <div className="line mb-2 " >
+           </div>
+           <div className="text row  position-relative ">
+                          <div className="  col-8 title">
+                              <h2 >{item.title} </h2> 
+                          </div>
+                          <div className='d-flex justify-content-end  col-4 clck '>
+                          {item.icon}
+                            <span className='align-self-center ps-2'> {item.time}  </span>
+                          </div>
+                          <p>
+                              {item.text}
+                          </p>
+            </div>
           </div>
-                  
-           
-          ))
-        }
-         
-    </div>
-  
-  </div>
-
-
        
-         
+      </div>
+          </div>
+          <div className="col-lg-5 col-md-12  div1 ">
+          <div className=" p-4  ">
+          <div className=" col-12 div1 ">
+          <h4>{item.name}</h4>
+          <div className="line mb-2 " >
+           </div>
+           <div className="text row position-relative  ">
+                          <div className="  col-lg-9 col-md-12 title ">
+                              <h4 >{item.title}</h4> 
+                          </div>
+                          <div className='d-flex justify-content-end  col-lg-3 col-md-12 clck '>
+                          {item.icon}
+                            <span className='align-self-center ps-2'> {item.time}  </span>
+                          </div>
+                          <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus cupiditate ipsa laborum, quae consequatur sed ab
+                      </p>
+            </div>
+          </div>
+          <div className=" col-12  div1 ">
+          <h4>{item.name}</h4>
+          <div className="line mb-2 " >
+           </div>
+           <div className="text row   ">
+                          <div className="  col-lg-9 col-md-12 title">
+                              <h4 >{item.title} </h4> 
+                          </div>
+                          <div className='d-flex justify-content-end  col-lg-3 col-md-12  '>
+                          {item.icon}
+                            <span className='align-self-center ps-2'> {item.time}  </span>
+                          </div>
+                          <p>
+                          repellat at repudiandae accusamus deleniti repellendus, facilis blanditiis provident laborum esse enim, in quaerat a?
+                      </p>
+            </div>
+          </div>
+       
+      </div>
       
-      
+         </div>
+
+                   
+      </div>
+              
+       
+      ))
+    }
+     
 </div>
+
+   
+ 
+   </div>
+
   
-    <Section2/>
-    </> 
+</div>
+
+
+</div> 
+  </div>
   );
 };
 
